7504d3a527ea436f7c5004def7e25045a9832c7c
mary-cpu <rivasmary922@gmail.com>
2021-04-04 16:35:57 -0600

    creacion del ticket

--- app/Controllers/Ventas.php
+++ app/Controllers/Ventas.php
@@ -7,17 +7,19 @@ use App\Models\VentasModel;
 use App\Models\TemporalCompraModel;
 use App\Models\DetalleVentaModel;
 use App\Models\ProductosModel;
+use App\Models\ConfiguracionModel;
 
 
 
 class Ventas extends BaseController
 {
-	protected $ventas, $temporal_compra, $detalle_venta, $productos;
+	protected $ventas, $temporal_compra, $detalle_venta, $productos, $configuracion;
 
 	public function __construct()
 	{
 		$this->ventas = new VentasModel();
 		$this->detalle_venta = new DetalleVentaModel();
+		$this->configuracion = new ConfiguracionModel();
 		helper(['form']);
 	}
 
@@ -47,8 +49,14 @@ class Ventas extends BaseController
 
 		$session = session();
 
-		$resultadoId = $this->ventas->insertarVenta($id_venta, $total, $session->id_usuario, 
-		$session->id_caja, $id_cliente, $forma_pago);
+		$resultadoId = $this->ventas->insertarVenta(
+			$id_venta,
+			$total,
+			$session->id_usuario,
+			$session->id_caja,
+			$id_cliente,
+			$forma_pago
+		);
 
 		$this->temporal_compra = new TemporalCompraModel();
 
@@ -70,6 +78,75 @@ class Ventas extends BaseController
 
 			$this->temporal_compra->eliminarCompra($id_venta);
 		}
-		//return redirect()->to(base_url() . "/ventas/muestraCompraPdf/" . $resultadoId);
+		return redirect()->to(base_url() . "/ventas/muestraTicket/" . $resultadoId);
+	}
+
+	function muestraTicket($id_venta)
+	{
+		$data['id_venta'] = $id_venta;
+		echo view('header');
+		echo view('ventas/ver_ticket', $data);
+		echo view('footer');
+	}
+
+	function generaTicket($id_venta)
+	{
+		$datosVenta = $this->ventas->where('id', $id_venta)->first();
+		$detalle_venta = $this->detalle_venta->select('*')->where('id_venta', $id_venta)->findAll();
+		$nombreTienda = $this->configuracion->select('valor')->where('nombre', 'tienda_nombre')->get()->getRow()->valor;
+		$direccionTienda = $this->configuracion->select('valor')->where('nombre', 'tienda_direccion')->get()->getRow()->valor;
+		$leyendaTicket = $this->configuracion->select('valor')->where('nombre', 'ticket_leyenda')->get()->getRow()->valor;
+
+		$pdf = new \FPDF('P', 'mm', array(80, 200));
+		$pdf->AddPage();
+		$pdf->SetMargins(5, 5, 5);
+		$pdf->SetTitle("Venta");
+		$pdf->SetFont('Arial', 'B', 10);
+		$pdf->Cell(60, 5, $nombreTienda, 0, 1, 'C');
+
+		$pdf->Image(base_url() . '/images/ft.jpeg', 5, 5, 20, 15, 'JPEG');
+
+		$pdf->SetFont('Arial', '', 9);
+		$pdf->Cell(60, 5, $direccionTienda, 0, 1, 'C');
+
+		$pdf->SetFont('Arial', '', 9);
+		$pdf->Cell(60, 5, $datosVenta['fecha_alta'], 0, 1, 'C');
+
+		$pdf->SetFont('Arial', 'B', 9);
+		$pdf->Cell(15, 5, utf8_decode('Ticket:'), 0, 0, 'L');
+		$pdf->SetFont('Arial', '', 9);
+		$pdf->Cell(30, 5, $datosVenta['folio'], 0, 1, 'C');
+
+		$pdf->Ln();
+
+		$pdf->SetFont('Arial', 'B', 7);
+		
+		$pdf->Cell(7, 5, 'Cant.', 0, 0, 'L');
+		$pdf->Cell(35, 5, 'Nombre', 0, 0, 'L');
+		$pdf->Cell(15, 5, 'Precio', 0, 0, 'L');
+		$pdf->Cell(15, 5, 'Importe', 0, 1, 'L');
+
+		$pdf->SetFont('Arial', '', 7);
+
+		$contador = 1;
+
+		foreach ($detalle_venta as $row) {
+			$pdf->Cell(7, 5, $row['cantidad'], 0, 0, 'L');
+			$pdf->Cell(35, 5, $row['nombre'], 0, 0, 'L');
+			$pdf->Cell(15, 5, $row['precio'], 0, 0, 'L');
+			$importe = number_format($row['precio'] * $row['cantidad'], 2, '.', ',');
+			$pdf->Cell(15, 5, 'C$' . $importe, 0, 1, 'R');
+			$contador++;
+		}
+
+		$pdf->Ln();
+		$pdf->SetFont('Arial', 'B', 8);
+		$pdf->Cell(70, 5, 'Total C$' . number_format($datosVenta['total'], 2, '.', ','), 0, 1, 'R');
+
+		$pdf->Ln();
+		$pdf->MultiCell(70, 5, $leyendaTicket, 0, 'C', 0);
+
+		$this->response->setHeader('Content-Type', 'application/pdf');
+		$pdf->Output("ticket.pdf", "I");
 	}
 }
